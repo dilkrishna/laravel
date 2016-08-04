8329d0d67e5710e2704944f5efc63b1c9a4da560
Pila <pila_dil@yahoo.com>
2016-08-04 18:10:14 +0545

    merged

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.master')
+@section('content')
+    <div class="row">
+        <div class="col-lg-10">
+            <h1> All the posts</h1>
+        </div>
+        <div class="col-lg-2">
+            <a href="{{ route('post.create') }}" class="btn btn-primary btn-h1-margin"> Create Post</a>
+        </div>
+    </div>
+    <h2>
+    <div class="row">
+        <table class="table table-striped table-hover ">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Body</th>
+                <th>Created At</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($posts as $post)
+            <tr>
+                <td>{{ $post->id }}</td>
+                <td>{{ $post->title }}</td>
+                <td>{{ substr($post->body, 0, 50)}}{{ strlen($post->body)>50 ? "..." :"" }}</td>
+                <td>{{ date( 'M j Y',strtotime($post->created_at)) }}</td>
+                <td><a href="{{ route('post.show',[$post->id]) }}" class="btn btn-inverse">view</a><a href="#"class="btn btn-inverse">edit</a> </td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+    </h2>
+
+
+@endsection
